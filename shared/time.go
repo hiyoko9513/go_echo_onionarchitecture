ee61fe392f49c49afc490ce285a2a3d305c9ff80
hiyoko <62996611+hiyoko9513@users.noreply.github.com>
2023-12-02 19:26:53 +0900

    :bug: 戻り値がなかった不具合

--- shared/time.go
+++ shared/time.go
@@ -7,12 +7,12 @@ import (
 var jst = time.FixedZone("Asia/Tokyo", 9*60*60)
 
 // Now time.Nowはserver依存の為、UTC固定としている
-func Now() {
-	time.Now().UTC()
+func Now() time.Time {
+	return time.Now().UTC()
 }
 
-func JstNow() {
-	time.Now().In(jst)
+func JstNow() time.Time {
+	return time.Now().In(jst)
 }
 
 func Timezone() (tz *time.Location) {
